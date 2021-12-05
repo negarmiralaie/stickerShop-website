3bc86a161ad9fe0d3c90befcb5f5e1681e69f3d2
“[negar <miralaienegar@gmail.com>
2021-12-06 02:07:49 +0330

    added some styles to cards

--- mimsticker.ir-newer/stickerShop-website/javascript/productCards.js
+++ mimsticker.ir-newer/stickerShop-website/javascript/productCards.js
@@ -1,6 +1,7 @@
 // const body = document.body;
 let productNo = 0;
-
+const bestSellesElement = document.getElementById("bestSelles");
+const bestSellProductsElement = document.getElementById("bestSellProducts");
 class productCard {
     constructor(productImgSrc, productName, productPrice) {
         this.productImgSrc = productImgSrc;
@@ -12,72 +13,77 @@ class productCard {
 }
 
 class placeCards {
-  constructor(parentSectionId) {
-    this.parentSectionId = parentSectionId;
-    this.parentSectionElement = document.createElement("section");
-    this.parentSectionElement.setAttribute("id", parentSectionId);
-    // this.parentSectionElement = document.getElementById(parentSectionId);
-  }
-  makeSectionWith4childSections(
-    productCard1,
-    productCard2,
-    productCard3,
-    productCard4
-  ) {
-    this.sectionWith4childSections = document.createElement("section");
-    this.sectionWith4childSections.classList.add("sectionWith4childSections");
-    this.makeSectionWith2childSections(productCard1, productCard2);
-    this.makeSectionWith2childSections(productCard3, productCard4);
-    console.log(this.parentSectionElement);
-    this.parentSectionElement.append(this.sectionWith4childSections);
-    body.append(this.parentSectionElement);
-  }
+    constructor(parentSectionId) {
+        this.parentSectionId = parentSectionId;
+        this.parentSectionElement = document.createElement("section");
+        this.parentSectionElement.setAttribute("id", parentSectionId);
+        // this.parentSectionElement = document.getElementById(parentSectionId);
+    }
+    makeSectionWith4childSections(
+        productCard1,
+        productCard2,
+        productCard3,
+        productCard4
+    ) {
+        this.sectionWith4childSections = document.createElement("section");
+        this.sectionWith4childSections.classList.add("sectionWith4childSections");
+        this.makeSectionWith2childSections(productCard1, productCard2);
+        this.makeSectionWith2childSections(productCard3, productCard4);
+        console.log(this.parentSectionElement);
+        this.parentSectionElement.append(this.sectionWith4childSections);
+        body.append(this.parentSectionElement);
+        // bestSellesElement;
+        // document.body.insertBefore(faqArticle, FolllowInstaAdd);
 
-  makeSectionWith2childSections(productCard1, productCard2) {
-    this.sectionWith2childSections = document.createElement("section");
-    this.sectionWith2childSections.classList.add("sectionWith2childSections");
-    this.make1ProductCard(productCard1);
-    this.make1ProductCard(productCard2);
-    this.sectionWith4childSections.append(this.sectionWith2childSections);
-    console.log(this.sectionWith4childSections);
-  }
+    }
 
-  makeProductImgElement(productCard) {
-    this.productImgElement = document.createElement("img");
-    this.productImgElement.src = productCard.productImgSrc;
-    this.productCard.append(this.productImgElement);
-  }
+    makeSectionWith2childSections(productCard1, productCard2) {
+        this.sectionWith2childSections = document.createElement("section");
+        this.sectionWith2childSections.classList.add("sectionWith2childSections");
+        this.make1ProductCard(productCard1);
+        this.make1ProductCard(productCard2);
+        this.sectionWith4childSections.append(this.sectionWith2childSections);
+        console.log(this.sectionWith4childSections);
+    }
 
-  makeProductNameElement(productCard) {
-    this.productNameElement = document.createElement("h5");
-    this.productNameElement.innerText = productCard.productName;
-    this.productCard.append(this.productNameElement);
-  }
+    makeProductImgElement(productCard) {
+        this.productImgElement = document.createElement("img");
+        this.productImgElement.src = productCard.productImgSrc;
+        this.productCard.append(this.productImgElement);
+    }
 
-  makeProductPriceElement(productCard) {
-    this.productPriceElement = document.createElement("h6");
-    this.productPriceElement.innerText = productCard.productPrice;
-    this.productCard.append(this.productPriceElement);
-  }
+    makeProductNameElement(productCard) {
+        this.productNameElement = document.createElement("p");
+        this.productNameElement.classList.add("productName");
+        this.productNameElement.innerText = productCard.productName;
+        this.productCard.append(this.productNameElement);
+    }
 
-  make1ProductCard(productCard) {
-    this.productCard = document.createElement("article");
-    this.productCard.classList.add("productCard");
-    this.productCard.setAttribute("id", `productCardNo${productNo}`);
-    this.makeProductImgElement(productCard);
-    this.makeProductNameElement(productCard);
-    this.makeProductPriceElement(productCard);
-    this.sectionWith2childSections.append(this.productCard);
-    productNo++;
-  }
+    makeProductPriceElement(productCard) {
+        this.productPriceElement = document.createElement("p");
+        this.productPriceElement.classList.add("productPrice");
+        this.productPriceElement.innerText = productCard.productPrice;
+        this.productCard.append(this.productPriceElement);
+    }
+
+    make1ProductCard(productCard) {
+        this.productCard = document.createElement("article");
+        this.productCard.classList.add("productCard");
+        this.productCard.setAttribute("id", `productCardNo${productNo}`);
+        this.makeProductImgElement(productCard);
+        this.makeProductNameElement(productCard);
+        this.makeProductPriceElement(productCard);
+        this.sectionWith2childSections.append(this.productCard);
+        productNo++;
+    }
 }
 
 let allStoredProductCards = [];
 
 let card1 = new productCard("../cardsImages/1-1.jpg", "استیکر عینک اسنوپ داگ", "۳٫۹۲۰تومان");
-let card2 = new productCard("../cardsImages/1-1.jpg", "استیکر دایناسور آفلاین", "۳٫۹۲۰تومان");
-let card3 = new productCard("../cardsImages/1-1.jpg", "استیکر شارژ 1درصد", "۳٫۹۲۰تومان");
-let card4 = new productCard("../cardsImages/1-1.jpg", "استیکر متن", "۳٫۹۲۰تومان");
+let card2 = new productCard("../cardsImages/2-1.jpg", "استیکر دایناسور آفلاین", "۳٫۹۲۰تومان");
+let card3 = new productCard("../cardsImages/3-1.jpg", "استیکر شارژ 1درصد", "۳٫۹۲۰تومان");
+let card4 = new productCard("../cardsImages/4-1.jpg", "استیکر متن", "۳٫۹۲۰تومان");
 
 allStoredProductCards.push(card1);
 allStoredProductCards.push(card2);
@@ -86,4 +92,3 @@ allStoredProductCards.push(card4);
 
 let placeCards1 = new placeCards("firstSectionWith4Childs");
 placeCards1.makeSectionWith4childSections(card1, card2, card3, card4);
-
