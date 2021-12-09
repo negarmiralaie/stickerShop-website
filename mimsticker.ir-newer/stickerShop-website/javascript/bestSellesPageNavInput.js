4414b5ca6d5490e6950661c61faedcfa43f07f23
“[negar <miralaienegar@gmail.com>
2021-12-09 21:02:36 +0330

    changed bg color of navigation colors

--- mimsticker.ir-newer/stickerShop-website/javascript/bestSellesPageNavInput.js
+++ mimsticker.ir-newer/stickerShop-website/javascript/bestSellesPageNavInput.js
@@ -1,3 +1,7 @@
+let bestSellesNextPageBtn = document.getElementById("bestSellesNextPageBtn");
+let bestSellesPreviousPageBtn = document.getElementById(
+  "bestSellesPreviousPageBtn"
+);
 //localStorage.clear();
 let bestSellesCurrentPage;
 let bestSellesPageNavInputElement = document.getElementById(
@@ -13,6 +17,14 @@ if (
     JSON.parse(localStorage.getItem("bestSellesCurrentPage"))
   );
   bestSellesPageNavInputElement.value = parseInt(bestSellesCurrentPage);
+  if (JSON.parse(localStorage.getItem("bestSellesCurrentPage")) == "1") {
+    bestSellesPreviousPageBtn.style.background = "gray";
+    bestSellesNextPageBtn.style.background = "#46a9e0";
+  }
+  if (JSON.parse(localStorage.getItem("bestSellesCurrentPage")) == "3") {
+    bestSellesNextPageBtn.style.background = "gray";
+    bestSellesPreviousPageBtn.style.background = "#46a9e0";
+  }
   showBestSellPages();
 } else {
   bestSellesCurrentPage = 1;
@@ -24,69 +36,65 @@ if (
 }
 
 let lastBestSellsPageNo = document.getElementById(
-    "lastBestSellsPageNo"
+  "lastBestSellsPageNo"
 ).innerText;
 
 // bestSellesPageNavInputElement.value = bestSellesCurrentPage;
-let bestSellesNextPageBtn = document.getElementById("bestSellesNextPageBtn");
-let bestSellesPreviousPageBtn = document.getElementById(
-    "bestSellesPreviousPageBtn"
-);
 
 function updateLocalStorageCurrentBestSellsPage() {
-    localStorage.setItem(
-        "bestSellesCurrentPage",
-        JSON.stringify(bestSellesPageNavInputElement.value)
-    );
-    bestSellesCurrentPage = bestSellesPageNavInputElement.value;  
+  localStorage.setItem(
+    "bestSellesCurrentPage",
+    JSON.stringify(bestSellesPageNavInputElement.value)
+  );
+  bestSellesCurrentPage = bestSellesPageNavInputElement.value;
 }
 
 bestSellesPageNavInputElement.addEventListener("input", (event) => {
-    updateLocalStorageCurrentBestSellsPage();
-    showBestSellPages();
+  updateLocalStorageCurrentBestSellsPage();
+  showBestSellPages();
 });
 
 //click next page
 bestSellesNextPageBtn.addEventListener("click", (event) => {
-    //if pageNo is max page no, next button should be disabled
-    if (
-        parseInt(bestSellesPageNavInputElement.value) + 1 ==
-        lastBestSellsPageNo
-    ) {
-        bestSellesNextPageBtn.style.cursor = "not-allowed";
-        bestSellesNextPageBtn.style.backgroundColor = "gray";
-    }
+  //if pageNo is max page no, next button should be disabled
+  if (
+    parseInt(bestSellesPageNavInputElement.value) + 1 ==
+    lastBestSellsPageNo
+  ) {
+    bestSellesNextPageBtn.style.cursor = "not-allowed";
+    bestSellesNextPageBtn.style.backgroundColor = "gray";
+  }
 
-    //if bestSellesPreviousPageBtn is clicked, go to previous page and change input value
-    if (
-      parseInt(bestSellesPageNavInputElement.value)!=
-      parseInt(lastBestSellsPageNo)
-    ) {
-      //if pageNo is not 1, increase input page number each time next btn is clicked
-      bestSellesPageNavInputElement.value =
-        parseInt(bestSellesPageNavInputElement.value) + 1;
-      bestSellesPreviousPageBtn.style.cursor = "pointer";
-      bestSellesPreviousPageBtn.style.backgroundColor = "#46a9e0";
-      updateLocalStorageCurrentBestSellsPage();
-      showBestSellPages();
-    }
+  //if bestSellesPreviousPageBtn is clicked, go to previous page and change input value
+  if (
+    parseInt(bestSellesPageNavInputElement.value) !=
+    parseInt(lastBestSellsPageNo)
+  ) {
+    //if pageNo is not 1, increase input page number each time next btn is clicked
+    bestSellesPageNavInputElement.value =
+      parseInt(bestSellesPageNavInputElement.value) + 1;
+    bestSellesPreviousPageBtn.style.cursor = "pointer";
+    bestSellesPreviousPageBtn.style.backgroundColor = "#46a9e0";
+    updateLocalStorageCurrentBestSellsPage();
+    showBestSellPages();
+  }
 });
 
 //click previous page
 bestSellesPreviousPageBtn.addEventListener("click", (event) => {
-    //if pageNo is 1, previous button should be disabled
-    if (parseInt(bestSellesPageNavInputElement.value) - 1 == 1) {
-        bestSellesPreviousPageBtn.style.cursor = "not-allowed";
-        bestSellesPreviousPageBtn.style.backgroundColor = "gray";
-    }
+  //if pageNo is 1, previous button should be disabled
+  if (parseInt(bestSellesPageNavInputElement.value) - 1 == 1) {
+    bestSellesPreviousPageBtn.style.cursor = "not-allowed";
+    bestSellesPreviousPageBtn.style.backgroundColor = "gray";
+  }
 
-    if (bestSellesPageNavInputElement.value != 1) {
-        //if pageNo is not max page number, decrease input page number each time previous btn is clicked
-        bestSellesPageNavInputElement.value =
-            parseInt(bestSellesPageNavInputElement.value) - 1;
-        bestSellesNextPageBtn.style.cursor = "pointer";
-        bestSellesNextPageBtn.style.backgroundColor = "#46a9e0";
-        updateLocalStorageCurrentBestSellsPage();
-        showBestSellPages();
-    }
+  if (bestSellesPageNavInputElement.value != 1) {
+    //if pageNo is not max page number, decrease input page number each time previous btn is clicked
+    bestSellesPageNavInputElement.value =
+      parseInt(bestSellesPageNavInputElement.value) - 1;
+    bestSellesNextPageBtn.style.cursor = "pointer";
+    bestSellesNextPageBtn.style.backgroundColor = "#46a9e0";
+    updateLocalStorageCurrentBestSellsPage();
+    showBestSellPages();
+  }
 });
